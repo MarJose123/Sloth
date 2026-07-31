22a49a2afbafdc397d280ea449892523bbcc231f
Marjose Darang <18107626+MarJose123@users.noreply.github.com>
2026-07-31 20:26:39 +0800

    updated unique id

--- app.config.ts
+++ app.config.ts
@@ -1,3 +1,14 @@
+/*
+ * Copyright (C) 2026
+ *
+ * Owner: MarJose123 (https://github.com/MarJose123/sloth)
+ * Project: Sloth
+ * License: GPLv3 <https://choosealicense.com/licenses/gpl-3.0/>
+ *
+ * Everyone is permitted to copy and distribute verbatim copies
+ *  of this license document, but changing it is not allowed.
+ */
+
 import { ExpoConfig, ConfigContext } from "expo/config";
 
 /**
@@ -17,7 +28,7 @@ const getUniqueIdentifier = () => {
     return "marjose.sloth.app.preview";
   }
 
-  return "marjose.sloth.app";
+  return "marjose.sloth.app.com";
 };
 
 const getAppName = () => {
