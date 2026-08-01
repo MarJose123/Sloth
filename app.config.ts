3104092ada0ee96b5128089068ae2fe395754f8f
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2026-08-01 08:13:00 +0000

    chore: bump version to 1.2.0 (build 144) [skip ci]

--- app.config.ts
+++ app.config.ts
@@ -47,7 +47,7 @@ export default ({ config }: ConfigContext): SlothExpoConfig => ({
   ...config,
   name: getAppName(),
   slug: "sloth",
-  version: "1.0.1",
+  version: "1.2.0",
   icon: "./assets/icons/icon.png",
   scheme: "sloth",
   userInterfaceStyle: "automatic",
@@ -132,5 +132,5 @@ export default ({ config }: ConfigContext): SlothExpoConfig => ({
     },
   },
   platforms: ["ios", "android"],
-  androidBuildNumber: 143,
+  androidBuildNumber: 144,
 });
